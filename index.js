8da61705d9122a269a02a967f63d47b43d551319
Andres <andresjml@gmail.com>
2021-04-01 12:34:50 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1=1
+let num2=62
+let multiply=num1*num2
+
+let random = Math.ceil(Math.random()*20);
+
+let num3=8
+let num4=12
+let mod = num4%num3
+
+let max = Math.max(8,20,15,19,3)
+
